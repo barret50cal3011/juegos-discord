8b9581c047db9831a02f3fa2b9e9bff8ceca7952
barret50cal <barret50cal3011@gmail.com>
2019-05-02 17:30:12 -0500

    Added JFrame and Board class

--- BiblicalUFOStarringMickeyMouse.java
+++ BiblicalUFOStarringMickeyMouse.java
@@ -1,6 +1,26 @@
+import javax.swing.JFrame;
+
 public class BiblicalUFOStarringMickeyMouse
 {
+	public JFrame frame;
+
+	public BiblicalUFOStarringMickeyMouse()
+	{
+		frame = new JFrame("Biblical UFO starring Miockey Mouse");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
 	public static void main(String[] args)
+	{
+		new BiblicalUFOStarringMickeyMouse();
+	}
+}
+
+class Board
+{
+	public Board()
 	{
 
 	}
