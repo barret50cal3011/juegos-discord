b440c75ca06c3cd83de6d90735fa21614df4cc87
barret50cal <barret50cal3011@gmail.com>
2019-05-02 18:29:28 -0500

    created the board object

--- BiblicalUFOStarringMickeyMouse.java
+++ BiblicalUFOStarringMickeyMouse.java
@@ -4,12 +4,16 @@ public class BiblicalUFOStarringMickeyMouse
 {
 	public JFrame frame;
 
+	public Board board;
+
 	public BiblicalUFOStarringMickeyMouse()
 	{
 		frame = new JFrame("Biblical UFO starring Miockey Mouse");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+
+		board = new Board();
 	}
 
 	public static void main(String[] args)
