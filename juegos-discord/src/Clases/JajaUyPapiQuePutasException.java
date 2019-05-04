502ea15f0154ab813df70323f10a496d5086bc78
Lele <nilocuru@hotmail.com>
2019-05-03 20:11:17 -0500

    Added comments and typesetting converted to Eclipse working set.

---
+++ juegos-discord/src/Clases/JajaUyPapiQuePutasException.java
@@ -0,0 +1,22 @@
+package Clases;
+
+/*
+ * Clase principal de excepción.
+ * Maneja TODAS las excepciones. Si no le gusta, por perdidas...
+ */
+public class JajaUyPapiQuePutasException extends Exception
+{
+	
+	/**
+	 * Constante de serialización.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/*
+	 * Metodo constructor de la clase JajaUyPapiQuePutasException.
+	 */
+	public JajaUyPapiQuePutasException()
+	{
+		super();
+	}
+}
