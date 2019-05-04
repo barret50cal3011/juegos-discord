7d070b1155e0111ec90e324c7c4ae31e7d92629a
barret50cal <barret50cal3011@gmail.com>
2019-05-03 22:08:00 -0500

    created a simple lancher, implemented actionEvent methods classes

--- juegos-discord/src/Clases/BiblicalUFOStarringMickeyMouse.java
+++ juegos-discord/src/Clases/BiblicalUFOStarringMickeyMouse.java
@@ -1,20 +1,33 @@
 package Clases;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 /*
  * Clase principal del juego
  * Maldita sea Daniel, comente su puto codigo...
  * Ups :v att: Daniel.
  */
-public class BiblicalUFOStarringMickeyMouse
+public class BiblicalUFOStarringMickeyMouse implements ActionListener
 {
+	//-------------------------------------------------------------
+		//Constantes
+	//-------------------------------------------------------------
+	public static final String LAUNCH = "Launch Game";
+
 	//-------------------------------------------------------------
 		//Atributos
 	//-------------------------------------------------------------
 	public JFrame frame;
 
-	public Board board;
-	
+	public JPanel panel;
+
+	public JButton bLaunch;
+
 	//-------------------------------------------------------------
 		//Bob el constructor
 	//-------------------------------------------------------------
@@ -26,11 +39,26 @@ public class BiblicalUFOStarringMickeyMouse
 	{
 		frame = new JFrame("Biblical UFO starring Mickey Mouse");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+		frame.setSize(new Dimension(500, 500));
 
-		board = new Board();
+		panel = new JPanel();
+
+		//Creacion y agregacion del boton
+		bLaunch = new JButton(LAUNCH);
+		bLaunch.setActionCommand(LAUNCH);
+		bLaunch.addActionListener(this);
+		panel.add(bLaunch);
+
+		frame.getContentPane().add(panel);
+
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setVisible(true);
 	}
+
+	//-------------------------------------------------------------
+		//Metodos
+	//-------------------------------------------------------------
 
 	/*
 	 * Metodo principal de inicio.
@@ -38,6 +66,19 @@ public class BiblicalUFOStarringMickeyMouse
 	public static void main(String[] args)
 	{
 		new BiblicalUFOStarringMickeyMouse();
+	}
+
+	/*
+	 *Metodo ejecutado al oprimir cualquier boton.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent evt)
+	{
+		String cmd = evt.getActionCommand();
+		if(cmd.equals(LAUNCH))
+		{
+			//TODO: launch method
+		}
 	}
 	
 }
