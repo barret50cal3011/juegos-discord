de5e9f3aaa9e4469b3a3a2419e59e2fc75e3c7da
Lele <nilocuru@hotmail.com>
2019-05-06 15:03:25 -0500

    Added Board class again UwU

---
+++ juegos-discord/src/ClientUI/Board.java
@@ -0,0 +1,23 @@
+package ClientUI;
+
+import javax.swing.JFrame;
+
+/*
+ * Clase donde de diagrama el tablero de juego.
+ */
+public class Board extends JFrame
+{
+
+	/**
+	 * Variable de serializacion
+	 */
+	private static final long serialVersionUID = 1L;
+
+	//--------------------------------------------
+		//Bob el constructor
+	//--------------------------------------------
+	public Board()
+	{
+		
+	}
+}
