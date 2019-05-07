b4768096fec9740a7985e2961dc19967fcb44646
Darkoyd <nilocuru@hotmail.com>
2019-05-07 11:32:56 -0500

    Board draws maps UwU. Test map added

--- juegos-discord/src/ClientUI/Board.java
+++ juegos-discord/src/ClientUI/Board.java
@@ -5,9 +5,12 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
+import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 
 import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
 import Clases.JajaUyPapiQuePutasException;
 import Client.Hex;
@@ -20,24 +23,63 @@ import Client.Point;
 public class Board extends JPanel 
 {
 
+	/*
+	 * Archivo de propiedades
+	 */
+	private Properties properties;
+	/*
+	 * Arreglo de arrglo de puntos de hexagonos
+	 */
+	private ArrayList<ArrayList<Point>> mapa;
+
+	private String nombre;
+
+	private int size;
+
+	private int origin;
+
+	private int hexes;
 
 	/**
 	 * Constante de serializacion.
 	 */
 	private static final long serialVersionUID = 1L;
+
 	
+
+	/*
+	 * Metodo constructor del tablero.
+	 */
+	public Board() throws JajaUyPapiQuePutasException
+	{
+		
+		properties = new Properties();
+		try
+		{
+			properties.load(new FileInputStream("./data/maps/testMap.properties"));
+		}
+		catch (Exception e)
+		{
+			throw new JajaUyPapiQuePutasException(e.getMessage());
+		}
+		mapa = new ArrayList<>();
+		
+		buildMap(properties);
+		setBorder(new TitledBorder(nombre));
+	}
+
 	/*
 	 * Metodo que agrega hexagonos al arreglo de puntos para dibujarlos.
 	 */
-	public ArrayList<Point> buildHexN(int i)
+	public ArrayList<Point> buildHexN(int q, int r, int s, int size, int origin)
 	{
 
 		try
 		{
-		Hex actualHex = new Hex(i, 0, -i);
-		Layout layout = new Layout(Layout.pointy, new Point(15,15), new Point(250,250));
-		ArrayList<Point> points = layout.polygonCorners(actualHex);
-		return points;
+			Hex actualHex = new Hex(q, r, s);
+			Layout layout = new Layout(Layout.pointy, new Point(size, size), new Point(origin, origin));
+			ArrayList<Point> points = layout.polygonCorners(actualHex);
+			return points;
 		}
 		catch (JajaUyPapiQuePutasException e)
 		{
@@ -47,9 +89,36 @@ public class Board extends JPanel
 	}
 
 	/*
+	 * Metodo que construye el mapa a partir del archivo de propiedades.
+	 */
+	public void buildMap(Properties properties) throws JajaUyPapiQuePutasException
+	{
+		nombre = properties.getProperty("nombre");
+
+		String x = properties.getProperty("size");
+		String y = properties.getProperty("origin");
+
+		size = Integer.parseInt(x);
+		origin = Integer.parseInt(y);
+		hexes = Integer.parseInt(properties.getProperty("hexes")); 
+
+
+		for(int i = 1; i <= hexes; i++)
+		{
+			String[] temp = properties.getProperty("hex"+i).split(",");
+			int q = Integer.parseInt(temp[0]); 
+			int r = Integer.parseInt(temp[1]); 
+			int s = Integer.parseInt(temp[2]); 
+			mapa.add(buildHexN(q, r, s, size, origin));
+		}
+
+
+	}
+
+	/*
 	 * Metodo que pinta un hexagono.
 	 */
-	public void paintTest(Graphics2D canvas, ArrayList<Point> points)
+	public void paintHex(Graphics2D canvas, ArrayList<Point> points)
 	{
 		canvas.setColor(Color.BLACK);
 		canvas.setStroke(new BasicStroke(1));
@@ -73,11 +142,11 @@ public class Board extends JPanel
 	{
 		super.paintComponent( canvas );
 
-		
-		for(int i = 0; i < 16 ; i++)
+
+		for(int i = 0; i < mapa.size(); i++)
 		{
 
-			paintTest((Graphics2D) canvas, buildHexN(i));
+			paintHex((Graphics2D) canvas, mapa.get(i));
 
 		}
 	}
